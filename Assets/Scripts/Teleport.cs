5711bbeb548c37f5f9f3276ce0ee02522622bc01
USER_1958 <wolf101x7@gmail.com>
2023-04-01 21:24:19 -0700

    Semi working teleport
    Body will teleport but camera wont follow

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    void OnTriggerEnter(Collider Col){
+        Col.transform.position = new Vector3 (Random.Range (-5.0f, 5.0f), Col.transform.position.y, Random.Range(-5.0f, 5.0f));
+    }
+}
