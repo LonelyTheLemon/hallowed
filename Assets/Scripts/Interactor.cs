e93ec74be60ecd68de8bb59243689d217fe2f57a
LonelyTheLemon <42897345+LonelyTheLemon@users.noreply.github.com>
2023-12-22 21:25:29 -0800

    Clean up interactor script a bit

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -5,16 +5,14 @@ interface IInteractable {
 }
 
 public class Interactor : MonoBehaviour {
-    [SerializeField]
-    Transform InteractorSource;
     [SerializeField, Range(1, 20)]
-    float InteractRange;
+    float range;
 
     void Update() {
         if(Input.GetKeyDown(KeyCode.E)) {
-            Ray ray = new Ray(InteractorSource.position, InteractorSource.forward);
-            if(Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange)) {
-                if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable obj)) {
+            Ray ray = new Ray(transform.position, transform.forward);
+            if(Physics.Raycast(ray, out RaycastHit rayHit, range)) {
+                if(rayHit.collider.gameObject.TryGetComponent(out IInteractable obj)) {
                     obj.Interact();
                 }
             }
