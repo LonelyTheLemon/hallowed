7b43495b17625f9929974a13e54b4d93cf899c1c
papawolfexe <wolf101x7@gmail.com>
2024-06-04 16:52:47 -0700

    Flashlight and Lighting
    added a spotlight for a flashlight and a flashlight script, also changed the scene lighting to be darker, lastly changed the tooltip text on the key and door

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject flashlight;
+    //public AudioSource On_Off;
+
+    bool on;
+    bool off;
+
+
+    void Start()
+    {
+        off = true;
+        flashlight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(off && Input.GetKeyDown(KeyCode.F)){
+            flashlight.SetActive(true);
+            //On_Off.Play();
+            off = false;
+            on = true;
+        }
+        else if (on && Input.GetKeyDown(KeyCode.F)){
+            flashlight.SetActive(false);
+            //On_Off.Play();
+            off = true;
+            on = false;
+        }
+    }
+}
