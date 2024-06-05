6c1de1f0255b7ffeada106e962d3024ed2f8a3df
papawolfexe <wolf101x7@gmail.com>
2024-06-04 20:39:16 -0700

    added sound effects into scene
    rain sound effect added to scene and flashlight sound effect added to scene, also organized environment stuff into a new empty

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public GameObject flashlight;
-    //public AudioSource On_Off;
+    public AudioSource On_Off;
 
     bool on;
     bool off;
@@ -22,13 +22,13 @@ public class Flashlight : MonoBehaviour
     {
         if(off && Input.GetKeyDown(KeyCode.F)){
             flashlight.SetActive(true);
-            //On_Off.Play();
+            On_Off.Play();
             off = false;
             on = true;
         }
         else if (on && Input.GetKeyDown(KeyCode.F)){
             flashlight.SetActive(false);
-            //On_Off.Play();
+            On_Off.Play();
             off = true;
             on = false;
         }
