4129eb18de30a702440553b3e3690c78824850c7
papawolfexe <wolf101x7@gmail.com>
2024-06-20 22:20:57 -0700

    Added sound effects to scene and key
    added environmental sound effects to scene and updated key script to  play key sound effect when grabbed

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,9 +5,11 @@ public class Key : MonoBehaviour, IInteractable, ITooltip {
     [SerializeField] string tooltip = "Key";
     public string Tooltip => tooltip;
     public static event Action KeyCollectEvent;
+    public AudioSource Grabbed;
     
     public void Interact() {
         KeyCollectEvent?.Invoke();
         Destroy(this.gameObject);
+        Grabbed.Play();
     }
 }
