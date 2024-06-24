fb83bfddaa223fe5345d4697194545bb38ef372b
LonelyTheLemon <42897345+LonelyTheLemon@users.noreply.github.com>
2024-06-23 20:04:41 -0700

    Make escape key unfocus cursor in build

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -7,27 +7,48 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
  
     float xRotation = 0f;
+    
+    KeyCode pauseKey = KeyCode.None;
  
-    //locks the mouse on to the screen
-    void Start()
-    {
+    void Start() {
+        // in unity editor you can hit escape in play mode to unfocus the mouse.
+        // this is undesirable behavior that can't be turned off, hence this workaround.
+        #if UNITY_EDITOR
+        pauseKey = KeyCode.BackQuote; // backquote is under escape key
+        #else
+        pauseKey = KeyCode.Escape; // normal escape in build
+        #endif
+
         Cursor.lockState = CursorLockMode.Locked;
     }
  
 
     void Update()
     {
-        //gets where the mouse is on the screen
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
- 
-        //moves the mouse onto the x and y plan and locks the y plane at 90 up and 90 down
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
- 
-        //player body rotation when moving mouse
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        if(Input.GetKeyDown(pauseKey)) {
+            switch(Cursor.lockState) {
+                case CursorLockMode.Locked:
+                    Cursor.lockState = CursorLockMode.None;
+                    break;
+                case CursorLockMode.None:
+                    Cursor.lockState = CursorLockMode.Locked;
+                    break;
+            }
+        }
+        
+        if(Cursor.lockState == CursorLockMode.Locked) {
+            //gets where the mouse is on the screen
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+     
+            //moves the mouse onto the x and y plan and locks the y plane at 90 up and 90 down
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+     
+            //player body rotation when moving mouse
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
     }
 }
 
