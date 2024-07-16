870b1947dc12099b4146a7a7dde189774d58a0b8
LonelyTheLemon <42897345+LonelyTheLemon@users.noreply.github.com>
2024-07-15 18:18:54 -0700

    Re-add light manager part of Game Manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,8 +8,34 @@ public class GameManager : MonoBehaviour {
     public static event Action AfterSleepPhaseSignal;
     
     void Start() {
+        setLightNight();
+
         // Any function that subscribes to AfterSleepPhaseSignal will be triggered
         // if SleepPhaseTrigger is called from Bed
         Bed.AfterSleepPhaseTrigger += AfterSleepPhaseSignal;
+        Bed.AfterSleepPhaseTrigger += setLightDay;
+    }
+    
+    // TODO: Move into proper light manager
+    Color nightColor = new Color(3 / 255, 3 / 255, 5 / 255, 1);
+    Color dayColor = new Color(1, 1, 1, 1);
+    [SerializeField] Transform[] lightObjects;
+
+    void setLightNight() {
+        foreach(Transform lightObject in lightObjects) {
+            Light light = lightObject.gameObject.GetComponent<Light>();
+            if(light != null) {
+                light.color = nightColor;
+            }
+        }
+    }
+
+    void setLightDay() {
+        foreach(Transform lightObject in lightObjects) {
+            Light light = lightObject.gameObject.GetComponent<Light>();
+            if(light != null) {
+                light.color = dayColor;
+            }
+        }
     }
 }
