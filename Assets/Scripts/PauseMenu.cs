e6bafe2529bd70d88375ab8ab72cfdd9aea429d8
LonelyTheLemon <42897345+LonelyTheLemon@users.noreply.github.com>
2024-09-15 19:30:22 -0700

    Make mouse sensitivity apply on load

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
-{
+public class PauseMenu : MonoBehaviour {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
     public GameObject optionsMenuUI; // Reference to the options menu UI
@@ -12,73 +10,77 @@ public class PauseMenu : MonoBehaviour
     
     KeyCode pauseKey = KeyCode.None;
     
+    public Slider mouseSensitivitySlider;
+    const string MouseSensitivityKey = "MouseSensitivity";
+    
     void Start() {
         #if UNITY_EDITOR
         pauseKey = KeyCode.BackQuote;
         #else
         pauseKey = KeyCode.Escape;
         #endif
+
+        // makes slider persistent with player settings
+        LoadSliderValue(mouseSensitivitySlider, MouseSensitivityKey);
+        mouseSensitivitySlider.onValueChanged.AddListener(value => UpdateSliderValue(MouseSensitivityKey, value));
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(pauseKey))
-        {
-            if (optionsMenuUI.activeSelf)
-            {
+    void Update() {
+        if (Input.GetKeyDown(pauseKey)) {
+            if (optionsMenuUI.activeSelf) {
                 CloseOptionsMenu();
             }
-            else if (GameIsPaused)
-            {
+            else if (GameIsPaused) {
                 Resume();
             }
-            else
-            {
+            else {
                 Pause();
             }
         }
     }
 
-    public void Resume()
-    {
+    void LoadSliderValue(Slider slider, string prefsKey) {
+        slider.value = PlayerPrefs.GetFloat(prefsKey);
+    }
+
+    public void UpdateSliderValue(string prefsKey, float value) {
+        PlayerPrefs.SetFloat(prefsKey, value);
+        PlayerPrefs.Save();
+    }
+
+    public void Resume() {
         pauseMenuUI.SetActive(false);
         GameIsPaused = false;
         EnableMouseLookScript();
     }
 
-    void Pause()
-    {
+    void Pause() {
         pauseMenuUI.SetActive(true);
         DisableMouseLookScript();
         GameIsPaused = true;
     }
 
-    public void LoadMenu()
-    {
+    public void LoadMenu() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         GameIsPaused = false;
     }
 
-    public void Quit()
-    {
+    public void Quit() {
         Application.Quit();
         Debug.Log("Player has Quit the game");
     }
 
-    public void OpenOptionsMenu()
-    {
+    public void OpenOptionsMenu() {
         optionsMenuUI.SetActive(true);
         pauseMenuUI.SetActive(false);
     }
 
-    public void CloseOptionsMenu()
-    {
+    public void CloseOptionsMenu() {
         optionsMenuUI.SetActive(false);
         pauseMenuUI.SetActive(true);
     }
 
-    private void EnableMouseLookScript()
-    {
+    void EnableMouseLookScript() {
         MouseLook mouseLookScript = mouseLookScriptObject.GetComponent<MouseLook>();
         if (mouseLookScript != null)
         {
@@ -88,8 +90,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    private void DisableMouseLookScript()
-    {
+    void DisableMouseLookScript() {
         MouseLook mouseLookScript = mouseLookScriptObject.GetComponent<MouseLook>();
         if (mouseLookScript != null)
         {
